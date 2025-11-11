04f01653c2eaa4e77c8fd86dc112179d0976bf37
ngxvu <xvung126@gmail.com>
2025-11-11 09:24:34 +0700

    Refactor Outbox model to include additional fields and rename file for clarity

---
+++ internal/models/outbox.go
@@ -0,0 +1,57 @@
+package models
+
+import (
+	"encoding/json"
+	"github.com/google/uuid"
+	"github.com/jinzhu/gorm/dialects/postgres"
+	"gorm.io/gorm"
+	"time"
+)
+
+type OutboxStatus string
+
+const (
+	OutboxStatusPending OutboxStatus = "pending"
+	OutboxStatusRetry   OutboxStatus = "retry"
+	OutboxStatusDone    OutboxStatus = "done"
+	OutboxStatusFailed  OutboxStatus = "failed"
+)
+
+type Outbox struct {
+	BaseModel
+	EventType     string          `json:"event_type" gorm:"type:varchar(100);index"`
+	Payload       *postgres.Jsonb `json:"payload" gorm:"type:jsonb"`
+	AggregateType string          `gorm:"size:100;not null"` // e.g. "order"
+	AggregateID   uuid.UUID       `gorm:"type:uuid;index"`   // order id
+	Status        OutboxStatus    `gorm:"size:20;not null;index"`
+	Attempts      int             `gorm:"not null;default:0"`
+	NextAttemptAt time.Time       `gorm:"index"`
+	ProcessedAt   *time.Time
+	UpdatedAt     time.Time
+}
+
+func (Outbox) TableName() string {
+	return "outboxes"
+}
+
+type OutboxEvent struct {
+	EventType     string          `json:"event_type"`
+	Payload       json.RawMessage `json:"payload"`
+	Process       bool            `json:"process"`
+	AggregateType string          `json:"aggregate_type"`
+	AggregateID   uuid.UUID       `json:"aggregate_id"`
+	Status        OutboxStatus    `json:"status"`
+	Attempts      int             `json:"attempts"`
+	NextAttemptAt time.Time       `json:"next_attempt_at"`
+}
+
+// BeforeCreate sets defaults
+func (o *Outbox) BeforeCreate(tx *gorm.DB) (err error) {
+	if o.ID == uuid.Nil {
+		o.ID = uuid.New()
+	}
+	if o.NextAttemptAt.IsZero() {
+		o.NextAttemptAt = time.Now()
+	}
+	return nil
+}
